8d7b9d1c55dbd9c67c4d23e57e9c997d7c6d14f4
JelleBe <jellebecir1999@gmail.com>
2023-01-16 13:40:09 +0100

    Created python script to filter variants using diffent methods.

---
+++ scripts/vcf_filter.py
@@ -0,0 +1,96 @@
+import argparse
+import sys
+import re
+
+___author__ = 'Jelle Becirspahic'
+___data__ = '16-01-2023'
+___version__ = 'v1.0'
+
+
+class VcfFilter:
+    def __init__(self, vcf_file, quality, depth, output):
+        self.vcf_file = open(vcf_file)
+        self.quality = quality
+        self.depth = depth
+        self.output = output
+
+    def quality_filter(self):
+        message = ""
+        with open(self.output, 'w') as new_file:
+            for line in self.vcf_file:
+                if line.startswith("#"):
+                    new_file.write(line)
+                try:
+                    if not line.startswith('#'):
+                        if int(line.split('\t')[5]) <= int(self.quality):
+                            new_file.write(line)
+                except ValueError:
+                    return 0
+                except TypeError:
+                    message += "fill in the -q"
+            if message != "":
+                print(message)
+            new_file.close()
+
+    def depth_filter(self):
+        with open(self.output, 'w') as new_file:
+            for line in self.vcf_file:
+                if line.startswith("#"):
+                    new_file.write(line)
+
+                if not line.startswith('#'):
+                    match = re.search("(DP=)(\d+)", str(line.split('\t')[7]))
+                    if int(match.group(2)) > int(self.depth):
+                        new_file.write(line)
+            new_file.close()
+
+    def decomposer_filter(self):
+        with open(self.output, 'w') as new_file:
+            for line in self.vcf_file:
+                if line.startswith("#"):
+                    new_file.write(line)
+
+                else:
+                    list_container = line.split('\t')
+                    if len(list_container[4].split(',')) > 1:
+                        for i in list_container[4].split(','):
+                            list_container[4] = i
+                            new_file.write('\t'.join([str(new_string)
+                                                      for new_string in list_container]))
+                    else:
+                        new_file.write(line)
+            new_file.close()
+
+        return 0
+
+
+def main(args):
+    inp_args = argparse.ArgumentParser()
+
+    inp_args.add_argument('method', help='Filter method to be used I.E. decompose, depth or quality')
+    inp_args.add_argument('-i', '--infile', help='Name of the input file, with filepath')
+    inp_args.add_argument('-q', '--quality', help='The upper limit of the quality of a variant.')
+    inp_args.add_argument('-d', '--depth', help='The minimum depth that a variant must have')
+    inp_args.add_argument('-o', '--outfile', help='Name of the output file, with filepath')
+
+    args = inp_args.parse_args()
+    method = args.method
+    vcf_file = args.infile
+    quality = args.quality
+    depth = args.depth
+    output = args.outfile
+
+    run = VcfFilter(vcf_file, quality, depth, output)
+
+    if method == "decompose":
+        run.decomposer_filter()
+    if method == "quality":
+        run.quality_filter()
+    if method == "depth":
+        run.depth_filter()
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
